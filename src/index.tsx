023df42591b1d9bce94a5f47086a3cc4f27f3b76
David AH <davidardila01@gmail.com>
2020-04-05 14:35:07 -0500

    Remove firebase keys

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,7 @@ import * as firebase from 'firebase'
 import * as serviceWorker from './serviceWorker';
 
 const config = {
-    apiKey: "AIzaSyAs2aYlz_L60Cpb3OQGhBsIbXiFif4wnx4",
-    authDomain: "react-chat-b954e.firebaseapp.com",
-    databaseURL: "https://react-chat-b954e.firebaseio.com",
-    projectId: "react-chat-b954e",
-    storageBucket: "react-chat-b954e.appspot.com",
-    messagingSenderId: "653743368725",
-    appId: "1:653743368725:web:8f07a7fa8d8d61c6737c1c"
+    
 }
 firebase.initializeApp(config)
 
